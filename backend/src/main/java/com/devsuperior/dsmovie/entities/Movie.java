0c7ea4b13984a3471586ab565e68f90bab81bc02
Saturno <samuelmathias033@gmail.com>
2022-03-13 20:37:36 -0300

    @samuelsaturn [Task: Save Score]

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.nio.MappedByteBuffer;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +23,11 @@ public class Movie {
 	private Double score;
 	private Integer count;
 	private String image;
+	
+	
+	@OneToMany(mappedBy = "id.movie", cascade = CascadeType.REMOVE)
+	
+	private Set<Score> scores = new HashSet<>();
 	
 	public Movie() {
 	}
@@ -68,6 +79,16 @@ public class Movie {
 	public void setImage(String image) {
 		this.image = image;
 	}
+
+	public Set<Score> getScores() {
+		return scores;
+	}
+
+	public void setScores(Set<Score> scores) {
+		this.scores = scores;
+	}
+	
+	
 
 	
 }
